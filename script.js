a33a3a4426b6d3420858f0f519616bcb1a0ecf8c
Varad Sandeep Naik <67594270+Hellacious1702@users.noreply.github.com>
2023-08-18 01:24:30 +0530

    Added BarCode Generation Functionality
    1) Added BarCode Generation Functionality
    ------Barcode Download Doesnt work .
    2) Made Some Styling Changes for the buttons and logo.

--- script.js
+++ script.js
@@ -6,29 +6,50 @@ let Card = document.querySelectorAll('.Card');
 let Generated = document.getElementById('generated');
 let DownloadBTN = document.getElementById('downloadBTN');
 let Download = document.getElementById('download');
+let Option = document.getElementById('option');
 
 // Creating and Storing Variables
 let qr = "";
 
-// The API Key
+// The API Key for QR
 let qrAPI = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data="
+
+// The API Key for BarCode
+let barAPI = "https://barcodeapi.org/api/39/";
 
 // Storing the string that needs to be attached to API Key to make it downloadable
 let downloadAPI = "&download=1";
 
 // Creating Function To Generate QR Code using API as per User Input  
 function Generate(){
-
-    // If block to check if the user has given some input or not
-    if(InputBox.value.length === 0){
-        alert("Please enter the URL");
+    // If Block to check if user wants to generate QR Code
+    if(Option.value === "QR"){
+        // If block to check if the user has given some input or not
+        if(InputBox.value.length === 0){
+            alert("Please enter the URL");
+        }
+        // Else Block to Generate QR Code if input box is not empty
+        else{
+            let qr = qrAPI + InputBox.value;
+            GeneratedQR.src = qr;
+            // Making The Generated DIV Visible by Changing CSS Property
+            Generated.style.display = "block";
+        }
     }
-    // Else Block to Generate QR Code if input box is not empty
-    else{
-        let qr = qrAPI + InputBox.value;
-        GeneratedQR.src = qr;
-        // Making The Generated DIV Visible by Changing CSS Property
-        Generated.style.display = "block";
+    // Else If Block to check if user wants to generate BAR Code
+    else if(Option.value === "BarCode"){
+        // If block to check if the user has given some input or not
+        if(InputBox.value.length === 0){
+            alert("Please enter the URL");
+        }
+        // Else Block to Generate QR Code if input box is not empty
+        else{
+            let UpperInput = InputBox.value.toUpperCase();
+            let qr = barAPI + UpperInput;
+            GeneratedQR.src = qr;
+            // Making The Generated DIV Visible by Changing CSS Property
+            Generated.style.display = "block";
+        }
     }
 }
    
@@ -39,6 +60,14 @@ GenerateBTN.addEventListener("click" , ()=>{
 
 // Listener for the Download Button that will push the download url to href of a in HTML
 DownloadBTN.addEventListener("click" , ()=>{
-    let qr = qrAPI + InputBox.value + downloadAPI;
-    Download.href = qr;
+    // If Block to check if user wants to Download QR Code
+    if(Option.value === "QR"){
+        let qr = qrAPI + InputBox.value + downloadAPI;
+        Download.href = qr;
+    }
+    // Else If Block to check if user wants to Download BAR Code
+    else if(Option.value === "BarCode"){
+        let Bar = "https://barcodeapi.org/api/auto/varad" + ".png";
+        Download.download = Bar;
+    }
 })
